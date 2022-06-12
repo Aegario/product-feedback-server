c90aeba30e472ee1d4d8c76026a711d8768c71c8
aQuere <ddenisshelmanov@gmail.com>
2022-06-12 15:25:02 +0300

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { ApolloServer } from 'apollo-server'
-import { schema } from 'schema'
+import { schema } from '@app/schema'
 
 const server = new ApolloServer({
-  schema
+  schema,
 })
 
 const port = 3000
@@ -10,4 +10,3 @@ const port = 3000
 server.listen({ port }).then(({ url }) => {
   console.log(`Server is ready at ${url}`)
 })
-
